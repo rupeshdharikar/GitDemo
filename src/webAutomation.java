8df9c1f7441d03b59bed26d8bdb010526e92ae91
Rupesh Dharikar <rupeshdharikar@gmail.com>
2024-05-10 17:23:51 +0530

    This is DevX

---
+++ src/webAutomation.java
@@ -0,0 +1,10 @@
+
+public class webAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI Practising for Git");
+		System.out.println("This is Rupesh Demo");
+	}
+
+}
